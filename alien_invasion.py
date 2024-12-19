e473410c547c0308a626a4c106359430df8205e1
emiliano <echiper.dev@gmail.com>
2024-12-19 08:55:23 +0200

    Set background color and apply through each loop pass

--- alien_invasion.py
+++ alien_invasion.py
@@ -13,6 +13,9 @@ class AlienInvasion:
         self.screen = pygame.display.set_mode((1200, 800))
         pygame.display.set_caption("Alien Invasion")
 
+        # Set the background color.
+        self.bg_color = (57, 53, 82)
+
     def run_game(self):
         """Start main game loop."""
         while True:
@@ -20,6 +23,9 @@ class AlienInvasion:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
+
+            # Redraw the screen during each pass through the loop.
+            self.screen.fill(self.bg_color)
 
             # Make the most recently drawn screen visible.
             pygame.display.flip()
