0793e449c6ed929662fa31659ed6b867b18a26b5
emiliano <echiper.dev@gmail.com>
2024-12-23 11:21:48 +0200

    Change activity status on game start to False

--- alien_invasion.py
+++ alien_invasion.py
@@ -33,7 +33,7 @@ class AlienInvasion:
         self._create_fleet()
 
         # Start Alien Invasion in an active state.
-        self.game_active = True
+        self.game_active = False
 
     def run_game(self):
         """Start main game loop."""
