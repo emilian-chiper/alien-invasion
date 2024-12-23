cecbbd8f7a00ad1e1f72bd8e49c2136afd5ad954
emiliano <echiper.dev@gmail.com>
2024-12-23 14:58:56 +0200

    Create method to increase ship, bullet, and alien speed

--- settings.py
+++ settings.py
@@ -32,3 +32,9 @@ class Settings:
         self.alien_speed = 1.0
         self.fleet_direction = 1
 
+    def increase_speed(self):
+        """Increase speed settings."""
+        self.ship_speed *= self.speedup_scale
+        self.bullet_speed *= self.speedup_scale
+        self.alien_speed *= self.speedup_scale
+
