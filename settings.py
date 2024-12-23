531cba73815dc2a630088e981b224514e4310274
emiliano <echiper.dev@gmail.com>
2024-12-23 14:57:32 +0200

    Create method to initialize dynamic game settings

--- settings.py
+++ settings.py
@@ -25,3 +25,10 @@ class Settings:
 
         self.initialize_dynamic_settings()
 
+    def initialize_dynamic_settings(self):
+        """Initialize settings that change throughout the game."""
+        self.ship_speed = 1.5
+        self.bullet_speed = 2.5
+        self.alien_speed = 1.0
+        self.fleet_direction = 1
+
