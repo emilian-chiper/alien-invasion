170136fd2d562eaab34cde4bcd5514c75f7c50ff
emiliano <echiper.dev@gmail.com>
2024-12-23 20:03:25 +0200

    Implement high scores

--- game_stats.py
+++ game_stats.py
@@ -10,4 +10,5 @@ class GameStats:
         """Initialize the statistics that can change during the game."""
         self.ships_left = self.settings.ship_limit
         self.score = 0
+        self.high_score = 0
 
